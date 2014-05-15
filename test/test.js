e2abbc3a6856f8cfb61ab12363e2fe329c4ca4c4
Mikola Lysenko <mikolalysenko@gmail.com>
2014-05-15 12:11:11 -0500

    adding test cases

---
+++ test/test.js
@@ -0,0 +1,12 @@
+"use strict"
+
+var tape = require("tape")
+var sc = require("simplicial-complex")
+var createTriangulation = require("../st.js")
+
+tape("spherical-triangulation", function(t) {
+
+  
+
+  t.end()
+})
